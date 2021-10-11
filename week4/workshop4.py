739ee14cc5b4bec1176803cf92840c2bc4ca925a
Saint. Clever <parchment@gmx.com>
2021-10-11 12:38:13 -0400

    workshop4.py update User subclass methods

--- week4/workshop4.py
+++ week4/workshop4.py
@@ -1,5 +1,20 @@
+'''
+Workshop 4
+'''
+
+
 class User:
-    '''User'''
+    '''
+    User Class
+    ----------
+    Returns:
+        User(name, pin, password)
+
+    functions:
+        change_name(self, name)
+        change_pin(self, pin)
+        change_password(self, password)
+    '''
 
     def __init__(self, name, pin, password):
         self.name = name
@@ -7,16 +22,25 @@ class User:
         self.password = password
 
     def change_name(self, name):
-        ''' change name '''
-        self.name = name
+        '''change name'''
+        if len(name) >= 2 and len(name) <= 10:  # Bonus Task 3
+            self.name = name
+        else:
+            print('Invalid username')
 
     def change_pin(self, pin):
-        ''' change pin '''
-        self.pin = pin
+        '''change pin'''
+        if len(str(pin)) == 4 and isinstance(pin, int):  # Bonus Task 3
+            self.pin = pin
+        else:
+            print('Invalid PIN')
 
     def change_password(self, password):
-        ''' change password '''
-        self.password = password
+        '''change password'''
+        if len(password) >= 5:
+            self.password = password  # Bonus Task 3
+        else:
+            print('Invalid password')
 
     def __repr__(self):
         '''Driver repr'''
@@ -25,26 +49,54 @@ class User:
 
 
 class BankUser(User):
-    '''BankUser'''
+    '''
+    BankUser Subclass:
+        of User class
 
-    def __init__(self, name, pin, password):
+    Returns:
+        show_balance(self)
+        withdraw(self, withdraw_amount)
+        deposit(self, deposit_amount)
+        transfer_money(self, transfer_amount, requestee)
+        request_money(self, requestee_amount, transferor)
+    '''
+
+    def __init__(self, name, pin, password, on_hold=False):
+        # def __init__(self, name, pin, password):
         super().__init__(name, pin, password)
         self.balance = float(0)
 
+        '''TODO: Toggle'''
+        self.on_hold = on_hold
+
     def show_balance(self):
-        ''' show balance '''
-        print(f'{self.name} has an account balance of: {self.balance}')
+        '''show balance'''
+        print(f'{self.name} has an account balance of: ${self.balance:.2f}')
 
     def withdraw(self, withdraw_amount):
-        ''' withdraw '''
-        self.balance -= float(withdraw_amount)
+        '''withdraw'''
+        # Bonus Task 1 && 2
+        try:
+            if self.balance >= withdraw_amount and self.balance > 0:
+                self.balance -= float(withdraw_amount)
+            else:
+                print('Insufficient funds, withdrawal canceled.')
+        except TypeError:
+            print('Invalid withdraw')
 
     def deposit(self, deposit_amount):
-        ''' deposit '''
-        self.balance += float(deposit_amount)
+        '''deposit'''
+        # Bonus Task 1 && 2
+        try:
+            if deposit_amount > 0 and float or deposit_amount > 0 and int:
+                self.balance += float(deposit_amount)
+            else:
+                print('Insufficient deposit, deposit canceled.')
+        except TypeError:
+            print('Invalid deposit')
 
     def transfer_money(self, transfer_amount, requestee):
-        ''' transfer money '''
+        '''transfer money'''
         print(
             f'\nYou are transferring ${transfer_amount} to {requestee.name}\
             \nAuthentication required'
@@ -56,25 +108,30 @@ class BankUser(User):
             requestee.balance += transfer_amount
             print(f'Transferring ${transfer_amount} to {requestee.name}')
             return True
+        print('Invalid PIN. Transaction canceled.')
         return False
 
     def request_money(self, requestee_amount, transferor):
-        ''' request money '''
+        '''request money'''
         print(
             f'\nYou are requesting ${requestee_amount} from {transferor.name}\
             \nUser authentication is required...'
         )
         transferor_pin = int(input(f'Enter {transferor.name}\'s PIN: '))
-        requestee_passord = input('Enter your password: ')
 
-        if transferor_pin == transferor.pin and requestee_passord == self.password:
-            self.balance += requestee_amount
-            transferor.balance -= requestee_amount
-            print(
-                f'Request authorized\
-                \n{transferor.name} sent ${requestee_amount}'
-            )
-            return True
+        if transferor_pin == transferor.pin:
+            requestee_passord = input('Enter your password: ')
+            if requestee_passord == self.password:
+                self.balance += requestee_amount
+                transferor.balance -= requestee_amount
+                print(
+                    f'Request authorized\
+                    \n{transferor.name} sent ${requestee_amount}'
+                )
+                return True
+            print('Invalid password. Transaction canceled.')
+            return False
+        print('Invalid PIN. Transaction canceled.')
         return False
 
     def __repr__(self):
@@ -83,19 +140,19 @@ class BankUser(User):
         return rep
 
 
-# ''' Driver Code for Task 1 '''
+# # ''' Driver Code for Task 1 '''
 # account_holder = User('Bob', 1234, 'password')
 # print(account_holder)
 
 
-# ''' Driver Code for Task 2 '''
+# # ''' Driver Code for Task 2 '''
 # account_holder.change_name('Bobby')
 # account_holder.change_pin(4321)
 # account_holder.change_password('newpassword')
 # print(account_holder)
 
 
-# ''' Driver Code for Task 3'''
+# # ''' Driver Code for Task 3'''
 # account_holder = BankUser('Bob', 1234, 'password')
 # print(account_holder)
 # print()
@@ -114,7 +171,7 @@ class BankUser(User):
 # account_holder.show_balance()
 
 
-''' Driver Code for Task 5'''
+# ''' Driver Code for Task 5'''
 bob = BankUser('Bob', 1234, 'password')
 alice = BankUser('Alice', 5678, 'alicepassword')
 
